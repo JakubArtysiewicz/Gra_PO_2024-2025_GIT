7f47fcd0642c521508f9ec5ef3bdeab3fdff0627
JakubArtysiewicz <jakub.artysiewicz@infotech.edu.pl>
2024-10-18 10:10:54 +0200

    Dodanie przycisków do dobierania, nie dobierania, dodanie funckje liczącą wartości kart u gracza i krupiera.

--- scenes/Game_losujacy.gd
+++ scenes/Game_losujacy.gd
@@ -1,5 +1,11 @@
 extends Node2D
 
+var polozone_pieniadze = 1000
+
+
+#var test_sprawdzania_wartosci_kart1 = ["karo",["piki",2],["piki",2],["piki",3],["piki",5],["piki","A"]] #13
+#var test_sprawdzenia_wartosci_kart2 = [["karo","A"],["karo","Q"],["karo",1]] #21
+
 #PIK = Piki (♠) – karty w kolorze czarnym = 0
 #KAR = Karo (♦) – karty w kolorze czerwonym = 1
 #KIE = Kier (♥) – karty w kolorze czerwonym = 2
@@ -12,13 +18,47 @@ var karty_trefl = [2,3,4,5,6,7,8,9,10,"A","J","Q","K"]
 
 var lista_dostepnych_kolorow = [0,1,2,3]
 
+var karty_krupiera = []
+var karty_gracza = []
+#
+#var wartosc_kart_krupiera = 0
+#var wartosc_kart_gracza = 0
+
 func _ready():
-	pass # Replace with function body.
+	
+	#print(sprawdzenie_wartosci_kart(test_sprawdzania_wartosci_kart1))
+	#print(sprawdzenie_wartosci_kart(test_sprawdzenia_wartosci_kart2))
+	
+	$"2x".visible = false
+	$Dobierz.visible = false
+	$Niedobierz.visible = false
+	# Replace with function body.
 
 func _process(delta):
 	pass
 	
-func losowanie_karty():
+func sprawdzenie_wartosci_kart(lista):
+	var wartosci_kart = 0
+	var ma_Asa = false
+	
+	for i in lista:
+		
+		if i[1] is int:
+			wartosci_kart += i[1]
+		if i[1] in ["J","K","Q"]:
+			wartosci_kart += 10
+		if i[1] in ["A"]:
+			ma_Asa = true
+			
+	if ma_Asa:
+		if wartosci_kart + 10 > 21:
+			wartosci_kart += 1
+		else:
+			wartosci_kart += 10
+	return(wartosci_kart)
+			
+		
+func losowanie_karty(lista):
 	
 	if karty_piki.size() == 0:
 		lista_dostepnych_kolorow.erase(0)
@@ -34,26 +74,61 @@ func losowanie_karty():
 	
 		if kolor_wylosowany == 0:
 			var liczba_wylosowana = randi() % karty_piki.size()
-			print("piki",karty_piki[liczba_wylosowana])
+			lista.append(["piki",karty_piki[liczba_wylosowana]])
+			#print("piki",karty_piki[liczba_wylosowana])
 			karty_piki.erase(karty_piki[liczba_wylosowana])
 		
 		if kolor_wylosowany == 1:
 			var liczba_wylosowana = randi() % karty_karo.size()
-			print("karo",karty_karo[liczba_wylosowana])
+			lista.append(["karo",karty_karo[liczba_wylosowana]])
+			#print("karo",karty_karo[liczba_wylosowana])
 			karty_karo.erase(karty_karo[liczba_wylosowana])
 	
 		if kolor_wylosowany == 2:
 			var liczba_wylosowana = randi() % karty_kier.size()
-			print("kier",karty_kier[liczba_wylosowana])
+			lista.append(["kier",karty_kier[liczba_wylosowana]])
+			#print("kier",karty_kier[liczba_wylosowana])
 			karty_kier.erase(karty_kier[liczba_wylosowana])
 	
 		if kolor_wylosowany == 3:
 			var liczba_wylosowana = randi() % karty_trefl.size()
-			print("Trefl",karty_trefl[liczba_wylosowana])
+			lista.append(["trefl",karty_trefl[liczba_wylosowana]])
+			#print("trefl",karty_trefl[liczba_wylosowana])
 			karty_trefl.erase(karty_trefl[liczba_wylosowana])
 			
 	else:
 		print(null)
 		
-func _on_gra_pressed():
-	losowanie_karty()
+func dobranie_na_start_krupier():
+	losowanie_karty(karty_krupiera)
+	losowanie_karty(karty_krupiera)
+
+func dobieranie_na_start_gracz():
+	losowanie_karty(karty_gracza)
+	losowanie_karty(karty_gracza)
+
+func _on_start_pressed():
+	
+	$"2x".visible = true
+	$Dobierz.visible = true
+	$Niedobierz.visible = true
+	
+	dobranie_na_start_krupier()
+	dobieranie_na_start_gracz()
+	
+	print("Karty gracza:", karty_gracza, sprawdzenie_wartosci_kart(karty_gracza))
+	print("Karty krupiera:", karty_krupiera, sprawdzenie_wartosci_kart(karty_krupiera))
+
+	$BlackjackStol/Start.visible = false 
+
+
+func _on_dobierz_pressed():
+	losowanie_karty(karty_gracza)
+	print(karty_gracza,sprawdzenie_wartosci_kart(karty_gracza))
+	if sprawdzenie_wartosci_kart(karty_gracza)>21:
+		_exit_tree()
+
+func _on_niedobierz_pressed():
+	if sprawdzenie_wartosci_kart(karty_gracza > karty_krupiera):
+		polozone_pieniadze+=polozone_pieniadze
+	print(polozone_pieniadze)
--- scripts/menu.gd
+++ scripts/menu.gd
@@ -3,9 +3,11 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	#$Money.text = /root/game/$MoneyGame.text
 	#var scene = load("res://scenes/game.tscn")
 	#get_tree().change_scene_to("res://scenes/game.tscn")
-	pass # Replace with function body.
+	#pass # Replace with function body.
+	pass
 
 func _on_start_pressed():
 	#var scene = load("res://scenes/game.tscn")
